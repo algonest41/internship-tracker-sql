fb4b030b7b8177b85a31b6cc8c59f3fbaf72af86
algonest41 <shreyawane123@gmail.com>
2026-05-27 19:24:59 +0530

    Add files via upload

---
+++ README.md.txt
@@ -0,0 +1,41 @@
+# Internship Tracker SQL Project
+
+This is a beginner SQL project made using MySQL.
+
+The project helps manage:
+- Students
+- Companies
+- Internship applications
+- Interview records
+
+## Technologies Used
+- SQL
+- MySQL Workbench
+
+## Tables Used
+- companies
+- students
+- applications
+- skills
+- interviews
+
+## SQL Concepts Used
+- CREATE TABLE
+- PRIMARY KEY
+- FOREIGN KEY
+- INSERT
+- SELECT
+- WHERE
+- ORDER BY
+- GROUP BY
+- UPDATE
+- DELETE
+- ALTER TABLE
+
+## Files
+- schema.sql
+- insert_data.sql
+- queries.sql
+
+## Purpose
+This project was created to practice basic SQL concepts and database operations.
---
+++ schema.sql
@@ -0,0 +1,40 @@
+CREATE DATABASE internship_tracker;
+USE internship_tracker;
+
+CREATE TABLE companies(
+company_id INT primary KEY,
+company_name VARCHAR(50) NOT NULL,
+location VARCHAR(100)
+);
+
+CREATE TABLE students(
+student_id INT PRIMARY KEY,
+student_name VARCHAR(50) NOT NULL,
+email VARCHAR(50) UNIQUE
+);
+
+CREATE TABLE applications(
+application_id INT PRIMARY KEY,
+student_id INT,
+company_id INT,
+apply_date DATE,
+FOREIGN KEY (student_id) REFERENCES students(student_id),
+FOREIGN KEY (company_id) REFERENCES companies(company_id)
+);
+
+CREATE TABLE skills(
+skill_id INT PRIMARY KEY,
+skill_name VARCHAR(50) NOT NULL
+);
+
+create TABLE interviews(
+interview_id INT PRIMARY KEY,
+application_id INT,
+round INT,
+result varchar(50),
+FOREIGN KEY (application_id) REFERENCES applications (application_is)
+);
+
+
+
+
